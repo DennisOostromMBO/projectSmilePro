aca208ef16ed67bbc9db94ef0d21497ce1b1ab21
Dynamiteboem2 <danielvangrol2006@gmail.com>
2024-11-20 14:35:56 +0100

    werkt eindelijk

--- resources/views/AccountOverzicht/AccountOverzicht.blade.php
+++ resources/views/AccountOverzicht/AccountOverzicht.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Account Overzicht</title>
+</head>
+<body>
+    Mehdi is NIGGER en terrorist
+</body>
+</html>
