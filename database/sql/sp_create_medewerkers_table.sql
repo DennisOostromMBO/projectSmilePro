ad590ab44bcdc50e67c1fd8e662fcd12dc69fff8
Alperen <79103497+Swendoz@users.noreply.github.com>
2024-11-20 13:20:20 +0100

    Stored procedure toegevoegd.

---
+++ database/sql/sp_create_medewerkers_table.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE IF NOT EXISTS create_medewerker_table()
+BEGIN
+    CREATE TABLE IF NOT EXISTS medewerkers (
+        Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY
+        ,PersoonId INT UNSIGNED NOT NULL
+        ,Nummer VARCHAR(255) NOT NULL
+        ,Medewerkertype VARCHAR(255) NOT NULL
+        ,Specialisatie VARCHAR(255) NOT NULL
+        ,Beschikbaarheid VARCHAR(255) NOT NULL
+        ,IsActive BIT DEFAULT 1
+        ,Comments VARCHAR(255) NULL
+        ,CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ,UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+END;
