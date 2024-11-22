92651c22129db844c668a203b370f04a34d67552
Dennis Oostrom <336914@student.mboutrecht.nl>
2024-11-22 11:43:55 +0100

    SQL beter gemaakt

--- database/sql/sp_create_contacten_table.sql
+++ database/sql/sp_create_contacten_table.sql
@@ -2,21 +2,20 @@ DROP PROCEDURE IF EXISTS create_contacten_table;
 CREATE PROCEDURE create_contacten_table()
 BEGIN
     CREATE TABLE IF NOT EXISTS contact (
-        Id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,  
-        PatientId BIGINT UNSIGNED NOT NULL,  
+        Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,  
+        PatientId INT UNSIGNED NOT NULL,  
         Straatnaam VARCHAR(255) NOT NULL,  
-        Huisnummer VARCHAR(10) NOT NULL,  
+        Huisnummer SMALLINT NOT NULL,  
         Toevoeging VARCHAR(10) NULL, 
         Postcode VARCHAR(10) NOT NULL,  
-        Plaats VARCHAR(255) NOT NULL,  
-        VolledigAdres VARCHAR(512) AS (CONCAT(Straatnaam, ' ', Huisnummer,  IF(Toevoeging IS NOT NULL AND Toevoeging != '', CONCAT('-', Toevoeging), ''),  ', ', Postcode, ' ',   Plaats  )) STORED,
+        Plaats VARCHAR(100) NOT NULL,  
+        VolledigAdres VARCHAR(255) AS (CONCAT(Straatnaam, ' ', Huisnummer,  IF(Toevoeging IS NOT NULL AND Toevoeging != '', CONCAT('-', Toevoeging), ''),  ', ', Postcode, ' ',   Plaats  )) STORED,
         Mobiel VARCHAR(20) NOT NULL,  
         Email VARCHAR(255) NOT NULL,  
-        IsActief BIT DEFAULT 1,  
+        IsActief BIT NOT NULL DEFAULT 1,  
         Opmerking VARCHAR(255) NULL,  
         DatumAangemaakt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
         DatumGewijzigd TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
     ) ENGINE=InnoDB;
 END;
- 
         
--- database/sql/sp_create_persoon_table.sql
+++ database/sql/sp_create_persoon_table.sql
@@ -7,7 +7,7 @@ BEGIN
         Achternaam VARCHAR(100) NOT NULL,
         VolledigeNaam VARCHAR(150) AS (CONCAT(Voornaam, ' ', IFNULL(Tussenvoegsel, ''), ' ', Achternaam)) STORED,
         Geboortedatum DATE NOT NULL,
-        IsActive BIT DEFAULT 1,
+        IsActive BIT NOT NULL DEFAULT 1,
         Comments VARCHAR(255) NULL,
         CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
