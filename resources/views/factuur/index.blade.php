b1e4cbe19b59fabd55ba14f2215b8e1680f39889
marsianss <150048927+marsianss@users.noreply.github.com>
2024-11-27 13:17:19 +0100

    Balzak

--- resources/views/factuur/index.blade.php
+++ resources/views/factuur/index.blade.php
@@ -30,6 +30,11 @@
         tr:hover {
             background-color: #f1f1f1;
         }
+        .no-data {
+            color: red;
+            font-weight: bold;
+            margin-top: 20px;
+        }
     </style>
 </head>
 <body>
@@ -37,6 +42,7 @@
         document.addEventListener('DOMContentLoaded', function() {
             var factuurIsEmpty = @json($factuur->isEmpty());
             if (factuurIsEmpty) {
+                alert("Geen factuur beschikbaar");
                 setTimeout(function() {
                     window.location.href = "{{ url('/') }}";
                 }, 4000);
@@ -44,24 +50,27 @@
         });
     </script>
     <h1>Factuur Overzicht</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Klant ID</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($factuur as $factuur)
+    @if ($factuur->isEmpty())
+        <p class="no-data">Geen factuur beschikbaar</p>
+    @else
+        <table>
+            <thead>
                 <tr>
-                    <td><a href="{{ route('factuur.show', $factuur->id) }}">{{ $factuur->id }}</a></td>
-                    <td>{{ $factuur->klant_id }}</td>
+                    <th>ID</th>
+                    <th>Klant ID</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($factuur as $factuur)
+                    <tr>
+                        <td><a href="{{ route('factuur.show', $factuur->id) }}">{{ $factuur->id }}</a></td>
+                        <td>{{ $factuur->klant_id }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 
     <a href="{{ url('/') }}" class="text-blue-500 hover:underline mb-4 inline-block">Terug naar Home</a>
 </body>
 </html>
-
