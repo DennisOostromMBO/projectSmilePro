870c0ab6d7151fafe552b980be9e53498d989a78
Fatih Kuzu <337920@student.mboutrecht.nl>
2024-12-04 14:14:44 +0100

    Afspraken Overzicht zit erin

---
+++ database/seeders/AfspraakSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Afspraak;
+
+class AfspraakSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Afspraak::create([
+            'gebruiker_id' => 1, // Verwijs naar een gebruiker die bestaat
+            'datum' => '2024-12-05', // Voorbeeld datum
+            'tijd' => '10:00', // Voorbeeld tijd
+            'notities' => 'Controle afspraak',
+        ]);
+
+        Afspraak::create([
+            'gebruiker_id' => 2, // Verwijs naar een gebruiker die bestaat
+            'datum' => '2024-12-06',
+            'tijd' => '14:00',
+            'notities' => 'Follow-up na operatie',
+        ]);
+
+        Afspraak::create([
+            'gebruiker_id' => 1,
+            'datum' => '2024-12-07',
+            'tijd' => '09:30',
+            'notities' => 'Bloedonderzoek voor allergieën',
+        ]);
+
+        Afspraak::create([
+            'gebruiker_id' => 3, // Verwijs naar een gebruiker die bestaat
+            'datum' => '2024-12-10',
+            'tijd' => '11:00',
+            'notities' => 'Informatie sessie voor nieuwe behandelingen',
+        ]);
+    }
+}
+$this->call(AfspraakSeeder::class);
